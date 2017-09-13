588c7c9bdf23284856e21d3b0a6c39efaf048c0e
shaoming Huang <msn8971@gmail.com>
2017-09-13 23:13:39 +0800

    ASP.NET

---
+++ test0913_test02/Default.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using System.Data;
+
+namespace test0913_test02 {
+    public partial class Default : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            string connString = WebConfigurationManager.ConnectionStrings["testConnectionString"].ConnectionString;
+            SqlConnection cn = new SqlConnection(connString);
+            cn.Open();
+            string cmdText = "select * from Products";
+            SqlCommand cmd = new SqlCommand(cmdText, cn);
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read()) {
+                Response.Write(dr["ProductName"]);
+            }
+            cmd.Cancel();
+            dr.Close();
+
+            if(cn.State == ConnectionState.Open) {
+                cn.Close();
+                cn.Dispose();
+            }
+            
+        }
+    }
+}
